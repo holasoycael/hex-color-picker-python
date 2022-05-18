5c9b23e9e40913764e6913e76a456c3da3e80c40
Natanael Lourenço <holasoycael@gmail.com>
2022-05-18 09:53:17 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+
+'''
+HEX Color Picker
+
+Usando o terminal desenvolver uma maneira de escrever a cor ou selecionar uma cor para então mostrar uma paleta de variações de da cor selecionada.
+
+'''
+
+print('This is HEX Color Picker')
